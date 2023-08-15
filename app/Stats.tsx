8177613f74311e9203a7435311a76be8f852d314
Zinbo <superzinbo@gmail.com>
2023-08-15 21:17:58 +0100

    Add custom scroll bars.

--- app/Stats.tsx
+++ app/Stats.tsx
@@ -7,7 +7,10 @@ export default function Stats() {
     return (
         <Section title="Stats">
 
-            <Box sx={{overflowX: "auto", width: "100%", }}>
+            <Box sx={{overflowX: "auto", width: "100%", "::-webkit-scrollbar" : {width: "10px"},
+                    "::-webkit-scrollbar-track": {borderRadius: "30px", backgroundColor: "#1E293B"},
+                "::-webkit-scrollbar-thumb": {borderRadius: "30px", backgroundColor: "#FFDE59"}}}>
+            {/*<Box sx={{overflowX: "auto", width: "100%"}}>*/}
                     <img src="http://ghchart.rshah.org/zinbo" alt="2016rshah's Github chart" style={{width: "100%", height: "100%", minWidth: "1400px"}}/>
             </Box>
         </Section>
